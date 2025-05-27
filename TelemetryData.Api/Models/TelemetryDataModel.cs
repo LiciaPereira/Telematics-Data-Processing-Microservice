a039bb966094eddc1ba0b40bcaae6365ecbd5a11
LiciaPereira <78990316+LiciaPereira@users.noreply.github.com>
2025-05-26 22:07:03 -0400

    initialize TelemetryDataModel and project structure

---
+++ TelemetryData.Api/Models/TelemetryDataModel.cs
@@ -0,0 +1,35 @@
+namespace TelemetryData.Api.Models
+{
+  public class TelemetryDataModel
+  {
+    public Guid VehicleId { get; set; }
+    public DateTime DateCreated { get; set; }
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
+    public float Speed { get; set; }
+    public EngineStatus EngineStatus { get; set; }
+
+    public TelemetryDataModel(
+      Guid vehicleId,
+      DateTime dateCreated,
+      decimal latitude,
+      decimal longitude,
+      float speed,
+      EngineStatus engineStatus
+      )
+    {
+      VehicleId = vehicleId;
+      DateCreated = dateCreated;
+      Latitude = latitude;
+      Longitude = longitude;
+      Speed = speed;
+      EngineStatus = engineStatus;
+    }
+  }
+  public enum EngineStatus
+  {
+    Off = 0,
+    Running = 1,
+    Idle = 2
+  }
+}
---
+++ TelemetryData.Tests/TelemetryDataControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using TelemetryData.Api.Models;
+
+namespace TelemetryData.Tests
+{
+
+  public class TelemetryDataModelTests
+  {
+    [Fact]
+    public void ConstructorShouldInitializeAllPropertiesCorrectly()
+    {
+      //arrange
+      var testVehicleId = Guid.NewGuid();
+      var testDateCreated = DateTime.UtcNow;
+      var testLatitude = -23.1816390M;
+      var testLongitude = -46.884170M;
+      var testSpeed = 75.0F;
+      var testEngine = TelemetryData.Api.Models.EngineStatus.Idle;
+
+      //act
+      var telemetry = new TelemetryDataModel(
+        testVehicleId,
+        testDateCreated,
+        testLatitude,
+        testLongitude,
+        testSpeed,
+        testEngine);
+
+      //assert
+      Assert.Equal(testVehicleId, telemetry.VehicleId);
+      Assert.Equal(testDateCreated, telemetry.DateCreated);
+      Assert.Equal(testLatitude, telemetry.Latitude);
+      Assert.Equal(testLongitude, telemetry.Longitude);
+      Assert.Equal(testSpeed, telemetry.Speed, 0.0001F);
+      Assert.Equal(testEngine, telemetry.EngineStatus);
+    }
+  }
+}
